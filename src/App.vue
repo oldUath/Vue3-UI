0c03929a6d2ca862233959ec1a25e6342fd9d085
oldUath <haiyangdong666@gmail.com>
2020-10-29 14:23:24 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi</div>
+<Dong />
+</template>
+
+<script>
+import Dong from './components/Dong'
+
+export default {
+    name: 'App',
+    components: {
+        Dong
+    }
+}
+</script>
