a952fc2fad2ce81dbcc1ed23249e35504f994818
oldUath <haiyangdong666@gmail.com>
2020-10-29 20:24:26 +0800

    路由切换之后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 
 <script lang="ts">
 import {
+    router
+} from './router'
+import {
     provide,
     ref
 } from 'vue'
@@ -16,7 +19,14 @@ export default {
 
         // 判断aside是否显示，把asideVisible的值封装到一个盒子里
         const asideVisible = ref(width <= 500 ? false : true);
-        provide('asideVisible', asideVisible) //set
+        provide('asideVisible', asideVisible); //set
+
+        //手机端路由切换后隐藏aside
+        router.afterEach(() => {
+            if (width <= 500) {
+                asideVisible.value = false;
+            }
+        })
     }
 }
 </script>
