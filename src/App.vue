33c9f5e003cfb4ed8a5c80cc9a969b105ef02684
oldUath <haiyangdong666@gmail.com>
2020-10-29 19:07:35 +0800

    实现aside的显示与隐藏

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,17 @@
 <router-view />
 </template>
 
-<script>
+<script lang="ts">
+import {
+    provide,
+    ref
+} from 'vue'
 export default {
     name: 'App',
-
+    setup() {
+        // 判断aside是否显示，把asideVisible的值封装到一个盒子里
+        const asideVisible = ref(false);
+        provide('asideVisible', asideVisible) //set
+    }
 }
 </script>
