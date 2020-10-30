44b3f45d71111f04da7dd00c8e74f4d397f28542
oldUath <haiyangdong666@gmail.com>
2020-10-30 13:26:15 +0800

    创建Button，添加插槽

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,14 @@
 <template>
 <div>
-    Button
-
+    <Button>你好</Button>
 </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+    components: {
+        Button
+    }
+}
+</script>
