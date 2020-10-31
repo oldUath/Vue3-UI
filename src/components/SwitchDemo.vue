fcd0ba32c04026266b82fa2399717b8a185f6ac9
oldUath <haiyangdong666@gmail.com>
2020-10-31 23:32:03 +0800

    展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -2,28 +2,28 @@
 <div>
     <h1>Switch 组件示例 </h1>
     <div class="demo">
-        <h2>支持 disabled </h2>
+        <h2>常规用法</h2>
         <div class="demo-component">
-            <Switch1Deom />
+            <Switch1Demo />
         </div>
         <div class="demo-actions">
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>&lt;Switch v-model:value="bool"  /&gt;</pre>
+            <pre>{{Switch1Demo.__sourceCode}}</pre>
         </div>
     </div>
 
     <div class="demo">
         <h2>支持 disabled </h2>
         <div class="demo-component">
-            <Switch2Deom />
+            <Switch2Demo />
         </div>
         <div class="demo-actions">
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+            <pre>{{Switch2Demo.__sourceCode}}</pre>
         </div>
     </div>
 </div>
@@ -32,8 +32,9 @@
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
-import Switch1Deom from './Switch1.demo.vue'
-import Switch2Deom from './Switch2.demo.vue'
+import Switch1Demo from './Switch1.demo.vue'
+import Switch2Demo from './Switch2.demo.vue'
+console.log(Switch1Demo.__sourceCode)
 import {
     ref
 } from 'vue'
@@ -41,13 +42,15 @@ export default {
     components: {
         Switch,
         Button,
-        Switch1Deom,
-        Switch2Deom,
+        Switch1Demo,
+        Switch2Demo,
     },
     setup() {
         const bool = ref(false);
         return {
-            bool
+            bool,
+            Switch1Demo,
+            Switch2Demo,
         }
     }
 }
