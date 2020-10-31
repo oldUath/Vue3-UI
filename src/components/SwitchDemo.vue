c4c75bb5a9d15073835099e0914c0573e04aeffd
oldUath <haiyangdong666@gmail.com>
2020-11-01 00:54:46 +0800

    展示源代码并且高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>{{Switch1Demo.__sourceCode}}</pre>
+            <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
         </div>
     </div>
 
@@ -23,7 +23,8 @@
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>{{Switch2Demo.__sourceCode}}</pre>
+            <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
+
         </div>
     </div>
 </div>
@@ -34,23 +35,26 @@ import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-console.log(Switch1Demo.__sourceCode)
+import 'prismjs';
+const Prism = (window as any).Prism
+import '../../node_modules/prismjs/themes/prism-okaidia.css'
 import {
     ref
 } from 'vue'
 export default {
     components: {
-        Switch,
         Button,
         Switch1Demo,
         Switch2Demo,
     },
     setup() {
         const bool = ref(false);
+
         return {
             bool,
             Switch1Demo,
             Switch2Demo,
+            Prism,
         }
     }
 }
