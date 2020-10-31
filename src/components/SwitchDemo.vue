780caf2d168d96e7c04640851bc27b1147d85651
oldUath <haiyangdong666@gmail.com>
2020-10-31 22:56:12 +0800

    抽离switchdemo组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,17 +1,48 @@
 <template>
 <div>
-    <Switch v-model:value="bool" />
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+        <h2>支持 disabled </h2>
+        <div class="demo-component">
+            <Switch1Deom />
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool"  /&gt;</pre>
+        </div>
+    </div>
+
+    <div class="demo">
+        <h2>支持 disabled </h2>
+        <div class="demo-component">
+            <Switch2Deom />
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        </div>
+    </div>
 </div>
 </template>
 
 <script lang="ts">
+import Switch from '../lib/Switch.vue'
+import Button from '../lib/Button.vue'
+import Switch1Deom from './Switch1.demo.vue'
+import Switch2Deom from './Switch2.demo.vue'
 import {
     ref
-} from 'vue';
-import Switch from '../lib/Switch.vue'
+} from 'vue'
 export default {
     components: {
-        Switch
+        Switch,
+        Button,
+        Switch1Deom,
+        Switch2Deom,
     },
     setup() {
         const bool = ref(false);
@@ -21,3 +52,38 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+
+.demo {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+
+    >h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $border-color;
+    }
+
+    &-component {
+        padding: 16px;
+    }
+
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+    }
+
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+
+        >pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+        }
+    }
+}
+</style>
