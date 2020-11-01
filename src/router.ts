29adb6102bcc9767a28750a08c0225c02ad99bdb
oldUath <haiyangdong666@gmail.com>
2020-11-01 18:41:23 +0800

    解决dist再网页上不能显示md文件

--- src/router.ts
+++ src/router.ts
@@ -5,22 +5,24 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import DocDemo from './components/DocDemo.vue'
-
 
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
+import intro from './markdown/intro.md'
+import getStarted from './markdown/getStarted.md'
+import install from './markdown/install.md'
+
 const history = createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
     history:history,
     routes:[
         {path:'/',component:Home},
         {path:'/doc',component:Doc,children:[
             {path:'',redirect:'/doc/intro'},
-            { path: "intro", component: md('intro') },
-            { path: "get-started", component: md('GetStarted') },
-            { path: "install", component: md('install') },
+            { path: "intro", component: md(intro) },
+            { path: "get-started", component: md(getStarted) },
+            { path: "install", component: md(install) },
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'Dialog',component:DialogDemo},
--- src/shims_vue.ts
+++ src/shims_vue.ts
@@ -2,4 +2,10 @@ declare module '*.vue' {
     import { ComponentOptions } from 'vue'
     const componentOptions: ComponentOptions
     export default componentOptions
+}  
+declare module '*.md' {
+  const str: string
+  export default str
   }
+
+  
