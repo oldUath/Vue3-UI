4e26e12a73951715e57e52da89c42bd81d1e567d
oldUath <haiyangdong666@gmail.com>
2020-11-01 15:21:16 +0800

    这个才是改造Tabs，上一个是改造Dialog

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,28 +1,20 @@
 <template>
 <div>
-    <h1>Tab组件</h1>
-    <Tabs v-model:selected="x">
-        <Tab title="导航1">内容1</Tab>
-        <Tab title="导航22222">内容2</Tab>
-    </Tabs>
+    <h1>Tabs 示例</h1>
+    <Demo :component="Tabs1Demo" />
 </div>
 </template>
 
 <script lang="ts">
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
-import {
-    ref
-} from 'vue'
+import Demo from './Demo.vue'
+import Tabs1Demo from './Tabs1.demo.vue'
 export default {
     components: {
-        Tabs,
-        Tab
+        Demo
     },
     setup() {
-        const x = ref("导航22222")
         return {
-            x
+            Tabs1Demo
         }
     }
 }
