4b5a740a04471ca13ffcc045d69f5802ac383c9d
oldUath <haiyangdong666@gmail.com>
2020-11-01 14:14:31 +0800

    优化markdown

--- src/router.ts
+++ src/router.ts
@@ -11,16 +11,19 @@ import Intro from './views/Intro.vue';
 import GetStarted from './views/GetStarted.vue'
 import Install from './views/Install.vue'
 
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 const history = createWebHashHistory()
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
     history:history,
     routes:[
         {path:'/',component:Home},
         {path:'/doc',component:Doc,children:[
             {path:'',component:DocDemo},
-            { path: "intro", component: Intro },
-            { path: "get-started", component: GetStarted },
-            { path: "install", component: Install },
+            { path: "intro", component: md('intro') },
+            { path: "get-started", component: md('GetStarted') },
+            { path: "install", component: md('install') },
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'Dialog',component:DialogDemo},
